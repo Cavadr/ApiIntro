14a4c01b50768461a8b2cd154bc01e49c9459c13
Cavadr <javadir@code.edu.az>
2022-02-17 07:38:44 +0400

    dto,category crud,

--- WebApi1/Controllers/ProductController.cs
+++ WebApi1/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi1.Data.DAL;
 using WebApi1.Data.Entity;
 
 namespace WebApi1.Controllers
@@ -12,6 +13,12 @@ namespace WebApi1.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+
+        private readonly Context _context;
+        public ProductController(Context context)
+        {
+            _context = context;
+        }
         public static List<Product> products = new List<Product> {
             new Product { Id = 1, Name = "Product1", Price = 50 },
             new Product { Id = 2, Name = "PRODUCT2", Price = 20 } };
@@ -30,9 +37,30 @@ namespace WebApi1.Controllers
             return StatusCode(200,product);
         }
 
-        //public IActionResult Update(Product product)
-        //{
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
 
-        //}
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+
+            Product dbproduct = _context.Products.FirstOrDefault(x => x.Id == product.Id);
+            if (dbproduct == null)
+            {
+                return NotFound();
+            }
+            dbproduct.Name = product.Name;
+            dbproduct.Price = product.Price;
+            _context.SaveChanges();
+            return StatusCode(200);
+
+
+        }
     }
 }
--- WebApi1/Data/DAL/Context.cs
+++ WebApi1/Data/DAL/Context.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi1.Data.Entity;
 
 namespace WebApi1.Data.DAL
 {
-    public class Context
+    public class Context:DbContext
     {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+              
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
